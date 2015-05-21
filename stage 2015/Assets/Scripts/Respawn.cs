1db98b7663334b86a5d8f85f091e3c4a4086393c
laktati <anas_x96@hotmail.fr>
2015-05-21 13:29:25 +0200

    Motorial Unity Project Folder

---
+++ stage 2015/Assets/Scripts/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Respawn : MonoBehaviour {
+
+	public GameObject Player;
+	public Transform RespawnPoint;
+	private Camera2DFollow cam;
+	private Transform CameraTarget;
+
+
+
+	void OnTriggerEnter2D(Collider2D col){
+		Destroy (col.gameObject);
+		Instantiate (Player, RespawnPoint.position, Quaternion.identity);
+		cam = Camera.main.GetComponent<Camera2DFollow>();
+		CameraTarget = GameObject.FindGameObjectWithTag ("Player").transform;
+		cam.target = CameraTarget;
+
+
+	}
+}
