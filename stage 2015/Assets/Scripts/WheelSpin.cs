18c07e6707683302b654cb2b6becd51b94a15b20
laktati <anas_x96@hotmail.fr>
2015-05-21 13:29:25 +0200

    no message

---
+++ stage 2015/Assets/Scripts/WheelSpin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelSpin : MonoBehaviour {
+	
+	public float speed = 1.0f;
+	public string axisName = "Horizontal";
+	
+
+	void Start () {
+	}
+	
+
+	void Update () {
+		
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			// Counter-clockwise
+			transform.Rotate(0, 0, 3.0f); 
+		}
+		
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			// clockwise
+			transform.Rotate(0, 0, -3.0f);
+		}
+		
+	}
+}
---
+++ stage 2015/Assets/Scripts/playerScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class playerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50,50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+
+
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			0);
+
+	
+		// 6 - Make sure we are not outside the camera bounds
+		var dist = (transform.position - Camera.main.transform.position).z;
+		
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).x;
+		
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)
+			).x;
+		
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).y;
+		
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)
+			).y;
+		
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+			transform.position.z
+			);
+		
+		// End of the update method
+
+
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+
+
+
+
+}
