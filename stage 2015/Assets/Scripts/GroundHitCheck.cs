556249720f30f38d989530a245091e08e27997f0
laktati <anas_x96@hotmail.fr>
2015-06-08 13:27:25 +0200

    no message

---
+++ stage 2015/Assets/Scripts/GroundHitCheck.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundHitCheck : MonoBehaviour {
+	
+	//flag to find out if the object is grounded
+	public GameObject go;
+	bool isGrounded = false;
+	private playerScript script;
+
+	//Empty gameobject created to determine the bounds/center of the object
+	public Transform GroundCheck1;
+	//public Transform GroundCheck2; //uncomment this for OverlapArea
+	
+	//The layer for which the overlap has to be detected 
+	public LayerMask ground_layers;
+
+	void Awake(){
+	
+		script = go.GetComponent<playerScript> ();
+	}
+
+	void Start(){
+	
+
+	}
+	//All the Physics related things to be done here
+	void FixedUpdate(){
+		//check if the empty object created overlaps with the 'ground_layers' within a radius of 1 units
+		isGrounded = Physics2D.OverlapCircle(GroundCheck1.position, 1, ground_layers);
+		Debug.Log ("Grounded: " + isGrounded);
+		if (!isGrounded){
+
+			script.enabled = false;
+		}
+		if (isGrounded){
+
+			script.enabled = true;
+		}	
+
+	}
+}
---
+++ stage 2015/Assets/Scripts/WheelJointBikeMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine; 
+using System.Collections; 
+
+
+public class WheelJointBikeMovement : MonoBehaviour { 
+	//reference to the wheel joints
+	WheelJoint2D[] wheelJoints; 
+	//center of mass of the car
+	public Transform centerOfMass;
+	//reference tot he motor joint
+	JointMotor2D motorBack;  
+	//horizontal movement keyboard input
+	float dir = 0f; 
+	//input for rotation of the car
+	float torqueDir = 0f;
+	//max fwd speed which the car can move at
+	float maxFwdSpeed = -5000;
+	//max bwd speed
+	float maxBwdSpeed = 2000f;
+	//the rate at which the car accelerates
+	float accelerationRate = 500;
+	//the rate at which car decelerates
+	float decelerationRate = -100;
+	//how soon the car stops on braking
+	float brakeSpeed = 2500f;
+	//acceleration due to gravity
+	float gravity = 9.81f;
+	//angle in which the car is at wrt the ground
+	float slope = 0;
+	//reference to the wheels
+	public Transform rearWheel;
+	public Transform frontWheel;
+
+	public int elevation = 5700;
+	
+	// Use this for initialization 
+	void Start () { 
+		//set the center of mass of the car
+		GetComponent<Rigidbody2D>().centerOfMass = centerOfMass.transform.localPosition;
+		//get the wheeljoint components
+		wheelJoints = gameObject.GetComponents<WheelJoint2D>(); 
+		//get the reference to the motor of rear wheels joint
+		motorBack = wheelJoints[1].motor; 
+	}  
+	
+	//all physics based assignment done here
+	void FixedUpdate(){
+		slope = transform.localEulerAngles.z;
+		dir = Input.GetAxis("Horizontal");
+		if (dir != 0) {
+			motorBack.motorSpeed += dir * -50;
+			wheelJoints [1].motor = motorBack;
+		} else {
+			motorBack.motorSpeed = 0;
+			wheelJoints [1].motor = motorBack;
+		}
+
+		dir = Input.GetAxis("Vertical");
+		if (dir != 0) {
+			print ("up");
+			frontWheel.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * elevation * Time.deltaTime);
+		}
+	}
+	
+}
