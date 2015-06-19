75afad52de260f9ebeb50aa5cf2772218788d52b
laktati <anas_x96@hotmail.fr>
2015-06-19 12:29:28 +0200

    add Level 2

--- stage 2015/Assets/Scripts/SelectLevel.cs
+++ stage 2015/Assets/Scripts/SelectLevel.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class SelectLevel : MonoBehaviour {
 
+	public string level1, level2, level3;
 
 	public void Level1(){
 
-		Application.LoadLevel ("TestScene");
+		Application.LoadLevel (level1);
 	}
 
 	public void Level2(){
-		Application.LoadLevel (null);
+		Application.LoadLevel (level2);
 	}
 
 	public void Level3(){
-		Application.LoadLevel (null);
+		Application.LoadLevel (level3);
 	}
 }
